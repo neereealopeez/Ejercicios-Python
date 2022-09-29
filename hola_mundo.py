7fe12b4d695c87d38b775aacd0b016a1d200da4b
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2022-09-29 10:03:12 +0200

    Comentarios, entrada, salida y tipos de datos en Python

---
+++ hola_mundo.py
@@ -0,0 +1,20 @@
+#Tipos de datos y variables
+
+from distutils.dep_util import newer
+
+
+nombre= "Nerea"
+edad= 17
+mayorOmenorEdad= False
+
+#Entrada y salida de datos.
+#Salida con print()
+
+print("Buenos días" , nombre , "tu edad es" , edad)
+
+
+#Entrada de datos
+nombre = input("Dime tu nombre: \n")
+edad= input("Dime tu edad \n")
+
+print("Buenos días" , nombre , "tu edad es" , edad)
