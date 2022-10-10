7f230c1b420a074b9dc3d463dd0e6797e8d1dd11
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2022-10-10 11:15:41 +0200

    Ejercicio sobre una agenda de teléfonos

---
+++ agenda de teléfonos.py
@@ -0,0 +1,22 @@
+'''
+Opción 1:
+Lista para nombres
+Lista para números de teléfono
+
+Opción 2:
+Lista para nombres y teléfonos
+Ej: (Juan - Teléfono, Pepe - Teléfono)
+
+'''
+#Opción 1
+vNombres = []
+vTeléfonos= []
+
+nombre= (input("Dime un nombre"))
+tele= (input("Dime un teléfono"))
+
+vNombres.append(nombre)
+vTeléfonos.append(tele)
+
+print(vNombres)
+print(vTeléfonos)
